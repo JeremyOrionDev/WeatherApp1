73643c1c3198aad8b91e4ee32bdd8a46aff95ee3
jeremyorionDev <jeremy9315@hotmail.fr>
2018-04-18 16:39:57 +0200

    Premier Commit

--- WeatherApp1/MainActivity.cs
+++ WeatherApp1/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using SharedProject1;
 
 namespace WeatherApp1
 {
@@ -13,6 +14,33 @@ namespace WeatherApp1
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.GetWeatherButton);
+
+            button.Click += delegate
+             {
+                 EditText ZipCodeEditText = FindViewById<EditText>(Resource.Id.ZipCodeEdit);
+
+                 Weather weather = Core.GetWeather(ZipCodeEditText.Text).Result;
+
+                 if (weather != null)
+                 {
+                     FindViewById<TextView>(Resource.Id.ResultsTitle).Text = weather.Title;
+                     FindViewById<TextView>(Resource.Id.TempText).Text = weather.Temperature;
+                     FindViewById<TextView>(Resource.Id.WindText).Text = weather.Wind;
+                     FindViewById<TextView>(Resource.Id.VisibilityText).Text = weather.Visibility;
+                     FindViewById<TextView>(Resource.Id.HumidityText).Text = weather.Humidity;
+                     FindViewById<TextView>(Resource.Id.SunriseText).Text = weather.Sunrise;
+                     FindViewById<TextView>(Resource.Id.SunsetText).Text = weather.Sunset;
+
+                     button.Text = "Search Again";
+
+                 }
+                 else
+                 {
+                     FindViewById<TextView>(Resource.Id.ResultsTitle).Text = "Couldn't find anything";
+                 }
+             };
         }
     }
 }
--- WeatherApp1/Resources/Resource.Designer.cs
+++ WeatherApp1/Resources/Resource.Designer.cs
@@ -41,6 +41,103 @@ namespace WeatherApp1
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040010
+			public const int HumidityText = 2130968592;
+			
+			// aapt resource value: 0x7f040005
+			public const int ResultsTable = 2130968581;
+			
+			// aapt resource value: 0x7f040004
+			public const int ResultsTitle = 2130968580;
+			
+			// aapt resource value: 0x7f040018
+			public const int SunriseText = 2130968600;
+			
+			// aapt resource value: 0x7f04001b
+			public const int SunsetText = 2130968603;
+			
+			// aapt resource value: 0x7f040008
+			public const int TempText = 2130968584;
+			
+			// aapt resource value: 0x7f040014
+			public const int VisibilityText = 2130968596;
+			
+			// aapt resource value: 0x7f04000c
+			public const int WindText = 2130968588;
+			
+			// aapt resource value: 0x7f040001
+			public const int ZipCodeLabel = 2130968577;
+			
+			// aapt resource value: 0x7f040003
+			public const int GetWeatherButton = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int ZipCodeEdit = 2130968578;
+			
+			// aapt resource value: 0x7f040000
+			public const int relativeLayout1 = 2130968576;
+			
+			// aapt resource value: 0x7f040006
+			public const int tableRow1 = 2130968582;
+			
+			// aapt resource value: 0x7f04000a
+			public const int tableRow2 = 2130968586;
+			
+			// aapt resource value: 0x7f04000e
+			public const int tableRow3 = 2130968590;
+			
+			// aapt resource value: 0x7f040012
+			public const int tableRow4 = 2130968594;
+			
+			// aapt resource value: 0x7f040016
+			public const int tableRow5 = 2130968598;
+			
+			// aapt resource value: 0x7f040019
+			public const int tableRow6 = 2130968601;
+			
+			// aapt resource value: 0x7f040007
+			public const int textView21 = 2130968583;
+			
+			// aapt resource value: 0x7f04000b
+			public const int textView22 = 2130968587;
+			
+			// aapt resource value: 0x7f04000f
+			public const int textView23 = 2130968591;
+			
+			// aapt resource value: 0x7f040009
+			public const int textView26 = 2130968585;
+			
+			// aapt resource value: 0x7f04000d
+			public const int textView27 = 2130968589;
+			
+			// aapt resource value: 0x7f040011
+			public const int textView28 = 2130968593;
+			
+			// aapt resource value: 0x7f040013
+			public const int textView34 = 2130968595;
+			
+			// aapt resource value: 0x7f040017
+			public const int textView40 = 2130968599;
+			
+			// aapt resource value: 0x7f04001a
+			public const int textView46 = 2130968602;
+			
+			// aapt resource value: 0x7f040015
+			public const int textView50 = 2130968597;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
